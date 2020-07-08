013bfcecd0f0f58134c41a5e4e0c9a2e1ab77d57
Prayha <jhj9512z@naver.com>
2020-07-08 14:09:47 +0900

    Logv
    logv

--- app/src/main/java/com/androidlec/addressbook/LoginActivity.java
+++ app/src/main/java/com/androidlec/addressbook/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -33,6 +34,8 @@ public class LoginActivity extends AppCompatActivity {
 
         btn_login.setOnClickListener(onClickListener);
         tv_register.setOnClickListener(onClickListener);
+
+        Log.v("LoginActivity", "login");
 
     }
 
