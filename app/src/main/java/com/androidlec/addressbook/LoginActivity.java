b17bd478771a8dc03abb716c871728942fcf683e
zzeung615 <zzeung615@gmail.com>
2020-07-08 16:12:59 +0900

    spinner

--- app/src/main/java/com/androidlec/addressbook/LoginActivity.java
+++ app/src/main/java/com/androidlec/addressbook/LoginActivity.java
@@ -2,7 +2,9 @@ package com.androidlec.addressbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -27,6 +29,15 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         init();
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/androidlec/addressbook/MainActivity.java
+++ app/src/main/java/com/androidlec/addressbook/MainActivity.java
@@ -3,12 +3,52 @@ package com.androidlec.addressbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.androidlec.addressbook.adapter_sh.CustomSpinnerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner_tags;
+    String[] spinnerNames;
+    int[] spinnerImages;
+    int selected_tag_idx = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner_tags = findViewById(R.id.main_sp_taglist);
+
+        spinnerNames = new String[]{"전체보기", "빨간색", "주황색", "노란색", "초록색", "파란색", "보라색", "회색"};
+        spinnerImages = new int[]{R.drawable.ic_tag_black, R.drawable.ic_tag_red, R.drawable.ic_tag_orange, R.drawable.ic_tag_yellow, R.drawable.ic_tag_green, R.drawable.ic_tag_blue, R.drawable.ic_tag_purple, R.drawable.ic_tag_gray};
+
+
+        CustomSpinnerAdapter customSpinnerAdapter = new CustomSpinnerAdapter(MainActivity.this, spinnerNames, spinnerImages);
+        spinner_tags.setAdapter(customSpinnerAdapter);
+
+
+        spinner_tags.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                selected_tag_idx = spinner_tags.getSelectedItemPosition();
+                Toast.makeText(MainActivity.this, spinnerNames[selected_tag_idx], Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
     }
 }
