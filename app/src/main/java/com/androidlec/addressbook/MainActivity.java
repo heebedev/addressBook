1f0444ad203740d6b70c526ca70410f88fb5f1e2
zzeung615 <zzeung615@gmail.com>
2020-07-08 18:31:54 +0900

    main and add Address

--- app/src/main/java/com/androidlec/addressbook/MainActivity.java
+++ app/src/main/java/com/androidlec/addressbook/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,6 +12,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.androidlec.addressbook.adapter_sh.CustomSpinnerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import android.util.Log;
 import android.view.Menu;
@@ -30,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     int[] spinnerImages;
     int selected_tag_idx = 0;
 
+    FloatingActionButton fladdBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         spinner_tags = findViewById(R.id.main_sp_taglist);
+        fladdBtn = findViewById(R.id.main_fab_add);
 
         spinnerNames = new String[]{"전체보기", "빨간색", "주황색", "노란색", "초록색", "파란색", "보라색", "회색"};
         spinnerImages = new int[]{R.drawable.ic_tag_black, R.drawable.ic_tag_red, R.drawable.ic_tag_orange, R.drawable.ic_tag_yellow, R.drawable.ic_tag_green, R.drawable.ic_tag_blue, R.drawable.ic_tag_purple, R.drawable.ic_tag_gray};
@@ -68,6 +73,9 @@ public class MainActivity extends AppCompatActivity {
 
         // 액션바
         actionBar.setTitle("내 주소록");
+
+        //floating Button
+        fladdBtn.setOnClickListener(onClickListener);
 
 
     }
@@ -119,6 +127,15 @@ public class MainActivity extends AppCompatActivity {
             // 텍스트 입력할 때마다 액션
             Log.v("Chance", "onQueryTextChange : " + newText);
             return false;
+        }
+    };
+
+
+    FloatingActionButton.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, AddActivity.class);
+            startActivity(intent);
         }
     };
 
