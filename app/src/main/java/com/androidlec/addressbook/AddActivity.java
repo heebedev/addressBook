22ccb4e43035a227f15cb95b88b806704fed0f05
TJ <tj@TJui-iMac-10.local>
2020-07-09 18:32:09 +0900

    tag select

--- app/src/main/java/com/androidlec/addressbook/AddActivity.java
+++ app/src/main/java/com/androidlec/addressbook/AddActivity.java
@@ -13,10 +13,12 @@ import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
@@ -25,10 +27,14 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidlec.addressbook.FTP_JHJ.ConnectFTP;
 import com.androidlec.addressbook.adapter_sh.CustomSpinnerAdapter;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -48,7 +54,9 @@ public class AddActivity extends AppCompatActivity {
 
     private Uri image_uri;
 
-
+    // Register
+    private int[] iv_tags = {R.id.add_iv_tagRed, R.id.add_iv_tagOrange, R.id.add_iv_tagYellow, R.id.add_iv_tagGreen, R.id.add_iv_tagBlue, R.id.add_iv_tagPurple, R.id.add_iv_tagGray};
+    private  ArrayList<String> tagList;
 
     private void init() {
         Resources res = getResources();
@@ -62,10 +70,22 @@ public class AddActivity extends AppCompatActivity {
         et_email = findViewById(R.id.et_addAddress_email);
         et_comment = findViewById(R.id.et_addAddress_cmt);
 
-        spinner_tags = findViewById(R.id.add_sp_taglist);
         spinnerNames = res.getStringArray(R.array.addaddresstaglist);
         spinnerImages = res.obtainTypedArray(R.array.tag_array);
 
+
+        for (int iv_tag : iv_tags) {
+            findViewById(iv_tag).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(v.isSelected()){
+                        v.setSelected(false);
+                    } else {
+                        v.setSelected(true);
+                    }
+                }
+            });
+        }
     }
 
     public boolean checkPermission() {
@@ -121,28 +141,7 @@ public class AddActivity extends AppCompatActivity {
 
         tvbtregister.setOnClickListener(onClickListener);
         tvbtcancle.setOnClickListener(onClickListener);
-
-
-        spinner_tags = findViewById(R.id.add_sp_taglist);
-
         ivAddImage.setOnClickListener(onClickListener);
-
-
-        CustomSpinnerAdapter customSpinnerAdapter = new CustomSpinnerAdapter(AddActivity.this, spinnerNames, spinnerImages);
-        spinner_tags.setAdapter(customSpinnerAdapter);
-
-        spinner_tags.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selected_tag_idx = spinner_tags.getSelectedItemPosition();
-                Toast.makeText(AddActivity.this, spinnerNames[selected_tag_idx], Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
 
     }
 
@@ -168,13 +167,66 @@ public class AddActivity extends AppCompatActivity {
         String phone = et_phone.getText().toString().trim();
         String email = et_email.getText().toString().trim();
         String comment = et_comment.getText().toString().trim();
+        if(tagSeletedOK()){
+            Log.e("Chance", tagList.toString());
+        } else {
+
+        }
 
         if(TextUtils.isEmpty(name)){
             Toast.makeText(this, "이름을 입력해 주세요.", Toast.LENGTH_SHORT).show();
         } else if(TextUtils.isEmpty(phone)){
             Toast.makeText(this, "전화번호를 입력해 주세요.", Toast.LENGTH_SHORT).show();
+        } else if(image_uri == null){
+            uploadToDB(name, phone, email, comment, "");
         } else {
-            Toast.makeText(this, "okok", Toast.LENGTH_SHORT).show();
+            //                                       context                    ip                  hostname          hostpw                port    uri(file)
+            ConnectFTP mConnectFTP = new ConnectFTP(AddActivity.this, "192.168.0.82", "host", "qwer1234", 25, image_uri);
+            String fileName = "";
+            try {
+                fileName = mConnectFTP.execute().get();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            uploadToDB(name, phone, email, comment, fileName);
+        }
+
+    }
+
+    private boolean tagSeletedOK() {
+
+        tagList = new ArrayList<>();
+
+        for (int i = 0; i < iv_tags.length; i++) {
+            if (findViewById(iv_tags[i]).isSelected()){
+                tagList.add(String.valueOf(i+1));
+            }
+        }
+
+        if (tagList.size() == 0){
+            tagList.add("0");
+            return true;
+        } else if(tagList.size() > 3){
+            Toast.makeText(this, "태그는 최대 3만 선택 가능합니다.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void uploadToDB(String name, String phone, String email, String comment, String fileName) {
+        String urlAddr = "http://192.168.0.79:8080/test/csAddAddressBook.jsp?";
+
+        urlAddr = urlAddr + "name=" + name + "&phone=" + phone + "&email=" + email + "&comment=" + comment + "&fileName=" + fileName;
+
+        try {
+            CSNetworkTask csNetworkTask = new CSNetworkTask(AddActivity.this, urlAddr);
+            csNetworkTask.execute().get(); // doInBackground 의 리턴값
+            finish();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -192,9 +244,10 @@ public class AddActivity extends AppCompatActivity {
                     } else {
                         pickFromGallery();
                     }
-                } else {
-                    Toast.makeText(AddActivity.this, "권한 요청을 동의해주세요.", Toast.LENGTH_SHORT).show();
                 }
+//                else {
+//                    Toast.makeText(AddActivity.this, "권한 요청을 동의해주세요.", Toast.LENGTH_SHORT).show();
+//                }
 
             }
         });
@@ -209,6 +262,7 @@ public class AddActivity extends AppCompatActivity {
                 assert data != null;
                 image_uri = data.getData();
             }
+
             Glide.with(this)
                     .load(image_uri.toString())
                     .apply(new RequestOptions().circleCrop())
